c0241e7e99c6703f145b9845708a7cadf8f1b868
glowies <comuokta@gmail.com>
2025-08-02 11:19:18 -0400

    feat: shaper space for ACEScg

--- ocio-lut-generator/generate-lut.sh
+++ ocio-lut-generator/generate-lut.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # ociobakelut --inputspace scene_linear --displayview 'sRGB - Display' 'ACES 2.0 - SDR 100 nits (Rec.709)' --format spi3d display-view.spi3d
 ociolutimage --generate --cubesize 32 --output clean32.exr
-ocioconvert --view clean32.exr scene_linear display-view32.exr 'sRGB - Display' 'ACES 2.0 - SDR 100 nits (Rec.709)'
+ocioconvert --view clean32.exr lin_ap1_shaper shaper-to-display-view32.exr 'sRGB - Display' 'ACES 2.0 - SDR 100 nits (Rec.709)' --gpuinfo
