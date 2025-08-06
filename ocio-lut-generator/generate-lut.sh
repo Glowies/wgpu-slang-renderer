91094b87506b6e6e0b8704f54ea97e361d1ee392
glowies <comuokta@gmail.com>
2025-08-05 21:20:05 -0400

    feat: use ktx tools to convert the LUT image to a ktx2

--- ocio-lut-generator/generate-lut.sh
+++ ocio-lut-generator/generate-lut.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 TEMP_DIR="./temp"
-OCIO="./studio-config-all-views-v2.3.0_aces-v2.0_ocio-v2.4.ocio"
+OCIO_CONFIG="./studio-config-all-views-v2.3.0_aces-v2.0_ocio-v2.4.ocio"
 INTER_FORMAT="png"
-CLEAN_EXR_LUT=${TEMP_DIR}/clean32.${INTER_FORMAT}
-FULL_EXR_LUT=${TEMP_DIR}/shaper-to-display-view32.${INTER_FORMAT}
+CLEAN_EXR_LUT=${TEMP_DIR}/clean32.exr
+FULL_EXR_LUT=${TEMP_DIR}/shaper-to-display-view32.exr
 OUTPUT_PREFIX="strip"
 NUM_SPLITS=32                      
 
@@ -15,24 +15,31 @@ STRIP_WIDTH=$NUM_SPLITS
 mkdir $TEMP_DIR
 
 # ociobakelut --inputspace lin_ap1_shaper --displayview 'sRGB - Display' 'ACES 2.0 - SDR 100 nits (Rec.709)' --cubesize 65 --format resolve_cube display-view.cube
-ociolutimage --generate --cubesize 32 --output $CLEAN_EXR_LUT
-ocioconvert --view $CLEAN_EXR_LUT lin_ap1_shaper $FULL_EXR_LUT 'sRGB - Display' 'ACES 2.0 - SDR 100 nits (Rec.709)'
+ociolutimage --generate --cubesize 32 --output $CLEAN_EXR_LUT --config $OCIO_CONFIG
+ocioconvert --view $CLEAN_EXR_LUT lin_ap1_shaper $FULL_EXR_LUT 'sRGB - Display' 'ACES 2.0 - SDR 100 nits (Rec.709)' --iconfig $OCIO_CONFIG
 
 echo "  Each strip will be ${STRIP_WIDTH} pixels wide."
 echo "  Starting image splitting..."
+
+ALL_SPLITS=""
 
 for i in $(seq 0 $((NUM_SPLITS - 1))); do
     # Calculate the starting Y-coordinate for the current strip
     X_START=$((i * STRIP_WIDTH))
 
     # Construct the output filename with zero-padding for consistent naming
-    OUTPUT_IMAGE="${OUTPUT_PREFIX}_$(printf "%02d" "$i").${INTER_FORMAT}"
+    OUTPUT_IMAGE="${TEMP_DIR}/${OUTPUT_PREFIX}_$(printf "%02d" "$i").${INTER_FORMAT}"
+    ALL_SPLITS="${ALL_SPLITS} ${OUTPUT_IMAGE}"
 
-    oiiotool "${FULL_EXR_LUT}" --cut "${STRIP_WIDTH}x${STRIP_WIDTH}+${X_START}+0}" -o "${TEMP_DIR}/${OUTPUT_IMAGE}"
+    oiiotool "${FULL_EXR_LUT}" --cut "${STRIP_WIDTH}x${STRIP_WIDTH}+${X_START}+0}" -d uint16 -o "${OUTPUT_IMAGE}"
 
     echo "  Created ${OUTPUT_IMAGE} (Y-range: ${Y_START} to $((Y_END - 1)))"
 done
 
 echo "  Image splitting complete! ${NUM_SPLITS} horizontal strips created."
 
-# rm -rf $TEMP_DIR
+# generate KTX2 image from the split images
+toktx --assign_oetf linear --zcmp --t2 --depth $NUM_SPLITS shaper_to_display32.ktx2 $ALL_SPLITS
+
+# clean up
+rm -rf $TEMP_DIR
