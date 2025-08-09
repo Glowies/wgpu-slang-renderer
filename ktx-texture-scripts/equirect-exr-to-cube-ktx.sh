d6e1045a26e799b582ea52268f7670625157054d
glowies <comuokta@gmail.com>
2025-08-09 08:36:40 -0400

    fix: cubemap face layout in ktx2 script

--- ktx-texture-scripts/equirect-exr-to-cube-ktx.sh
+++ ktx-texture-scripts/equirect-exr-to-cube-ktx.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 TEMP_DIR="./temp"
-INPUT_EXR=$1
-OUTPUT_KTX=$2
+CUBE_RESOLUTION=$1
+INPUT_EXR=$2
+OUTPUT_KTX=$3
 FACE_PREFIX="face"
 
 if [ -z "$1" ]; then
-  echo "ERROR: Argument 1 is missing. Argument 1 should be the input .exr file with long-lat equirectangular projection."
+  echo "ERROR: Argument 1 is missing. Argument 1 should be the resolution of the cubemap."
   exit 1
 fi
 
 if [ -z "$2" ]; then
-  echo "ERROR: Argument 2 is missing. Argument 2 should be the output .ktx2 file."
+  echo "ERROR: Argument 2 is missing. Argument 2 should be the input .exr file with long-lat equirectangular projection."
+  exit 1
+fi
+
+if [ -z "$3" ]; then
+  echo "ERROR: Argument 3 is missing. Argument 3 should be the output .ktx2 file."
   exit 1
 fi
 
@@ -19,7 +25,7 @@ mkdir $TEMP_DIR/tiled
 mkdir $TEMP_DIR/scanline
 
 TEMP_CUBE_FACE="${TEMP_DIR}/tiled/${FACE_PREFIX}%.exr"
-exrenvmap -w 256 $INPUT_EXR $TEMP_CUBE_FACE
+exrenvmap -w $CUBE_RESOLUTION $INPUT_EXR $TEMP_CUBE_FACE
 
 # output from `exrenvmap` are tiled exr files but `ktx create` gets a segfault
 # with tiled exrs. So we need to convert the exrs to scanline instead
@@ -30,7 +36,12 @@ do
   oiiotool $file --scanline -o $OUT_PATH
 done
 
-CUBE_FACES="${TEMP_DIR}/scanline/*"
+CUBE_FACES="${CUBE_FACES} ${TEMP_DIR}/scanline/${FACE_PREFIX}+X.exr"
+CUBE_FACES="${CUBE_FACES} ${TEMP_DIR}/scanline/${FACE_PREFIX}-X.exr"
+CUBE_FACES="${CUBE_FACES} ${TEMP_DIR}/scanline/${FACE_PREFIX}+Y.exr"
+CUBE_FACES="${CUBE_FACES} ${TEMP_DIR}/scanline/${FACE_PREFIX}-Y.exr"
+CUBE_FACES="${CUBE_FACES} ${TEMP_DIR}/scanline/${FACE_PREFIX}-Z.exr"
+CUBE_FACES="${CUBE_FACES} ${TEMP_DIR}/scanline/${FACE_PREFIX}+Z.exr"
 ktx create --format R16G16B16A16_SFLOAT --zstd 20 --assign-primaries srgb --assign-tf linear --cubemap $CUBE_FACES $OUTPUT_KTX
 
 rm -rf $TEMP_DIR
