a34c25764199e88e9cc8a9db58b13f057c0f6fbc
glowies <self@oktaycomu.com>
2025-08-14 20:48:40 -0400

    feat: add sh coefficient generation to cubemap script

--- ktx-texture-scripts/equirect-exr-to-cube-ktx.sh
+++ ktx-texture-scripts/equirect-exr-to-cube-ktx.sh
@@ -44,4 +44,7 @@ CUBE_FACES="${CUBE_FACES} ${TEMP_DIR}/scanline/${FACE_PREFIX}-Z.exr"
 CUBE_FACES="${CUBE_FACES} ${TEMP_DIR}/scanline/${FACE_PREFIX}+Z.exr"
 ktx create --format R16G16B16A16_SFLOAT --zstd 20 --assign-primaries srgb --assign-tf linear --cubemap $CUBE_FACES $OUTPUT_KTX
 
+# generate and store Spherical Harmonics coefficients for the cubemap
+cargo run -r -p sh-coefficient-baker -- $CUBE_FACES ${OUTPUT_KTX}.bin
+
 rm -rf $TEMP_DIR
